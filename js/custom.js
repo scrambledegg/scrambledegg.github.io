757fdc668f86ce9f541cfe219d44ed7bc67b4cf4
blyoa <blyoa110@gmail.com>
2017-05-21 08:31:54 +0000

    Update

--- js/custom.js
+++ js/custom.js
@@ -2,11 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
     // for footnote
     Array.prototype.forEach.call(document.querySelectorAll("a[rel='footnote']"), function(item) {
         let href = item.getAttribute("href");
-        if (item.href == null)
+        if (href == null)
             return;
         let target = document.getElementById(href.slice(1));
-        console.log(href.slice(1))
-        console.log(target)
         if (target == null)
             return;
         target.style.marginTop = "-100px";
